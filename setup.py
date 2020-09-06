89be7e2fccbca32cb0a552e73aebef4e4c6bf1fb
Victor Natschke <vnatschke@gmail.com>
2020-09-06 18:37:24 -0400

    Updated setup description

--- setup.py
+++ setup.py
@@ -3,12 +3,18 @@ from setuptools import setup, find_packages
 
 info = {}
 current_dir = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(current_dir, "README.md"), "r") as fh:
+    long_description = fh.read()
+
 with open(os.path.join(current_dir, "feedler", "__meta__.py"), "r") as file:
     exec(file.read(), info)
 
 setup(
     name=info["__title__"],
     description=info["__description__"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     version=info["__version__"],
     author=info["__author__"],
     author_email=info["__author_email__"],
